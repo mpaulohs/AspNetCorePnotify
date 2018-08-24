70564a161415fdf16d6cdb741838459a6d652e27
Marcos Paulo Honorato da Silva <mpaulohs@outlook.com>
2018-08-24 16:53:08 -0300

    New pnotify

---
+++ AspNetCoreWithPnotify/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWithPnotify.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+        public bool SimularException { get; set; }
+    }
+}
---
+++ AspNetCoreWithPnotify/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreWithPnotify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
